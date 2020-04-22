ced1c78e1684168e40870c657fbdc790fcd39da2
Trek5200 <singletrackgator@gmail.com>
2020-04-22 17:29:47 -0500

    Updating with Spring Boot framework

---
+++ src/test/java/org/launchcode/Tour/TourApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
