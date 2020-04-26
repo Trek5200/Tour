aed0980bbbe3f09c7130aca0866e69fd9621d752
Trek5200 <singletrackgator@gmail.com>
2020-04-26 16:03:35 -0500

    Login with User and Admin roles

---
+++ src/main/java/org/launchcode/Tour/MvcConfig.java
@@ -0,0 +1,18 @@
+package org.launchcode.Tour;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");    //landing page
+        registry.addViewController("/").setViewName("home");        //landing page
+        registry.addViewController("/hello").setViewName("hello");  //home page when logged in
+        registry.addViewController("/login").setViewName("login");  //login page
+        registry.addViewController("/user").setViewName("user");    //user accessible page
+        registry.addViewController("/admin").setViewName("admin");  //admin only accessible page
+    }
+}
---
+++ src/main/java/org/launchcode/Tour/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package org.launchcode.Tour;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("Admin")
+                .password("Admin")
+                .roles("ADMIN")
+                .and()
+                .withUser("User")
+                .password("User")
+                .roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
